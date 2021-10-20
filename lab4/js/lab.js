478db224c3403e8419461fb41578fee8478231e9
AvianBen <34182986+AvianBen@users.noreply.github.com>
2021-10-20 15:42:12 -0700

    Lab 4
    Completed Lab 4.

---
+++ lab4/js/lab.js
@@ -0,0 +1,10 @@
+//@author Benjamin Chavez
+//Pseudocode a Computer Task: Creating a Photoshop Document
+  //(optional) Download Photoshop by pressing the "Download" button next to Photoshop
+    //choose a directory for it to stay
+  //Open Adobe Creative Cloud app for desktop
+  //Open Photoshop by pressing the “Open” button on the cloud
+  //Press “Create New” on the left hand side of the screen
+  //On the right side of the pop-up window, set the width and height parameters to desired size(for ex. Choose “Pixels” and set the width to 2000 and height to 2000)
+  //Change the Resolution to 320
+  //Press the “Create” button
